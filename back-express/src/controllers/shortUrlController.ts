0f5b21a768581c412b42b4bb9e2011c88e2d0572
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 01:07:01 +0200

    refactor: Refactor shortUrlController.ts to use shortUrl service and handleError utility

--- back-express/src/controllers/shortUrlController.ts
+++ back-express/src/controllers/shortUrlController.ts
@@ -1,90 +1,38 @@
 import { Request, Response } from 'express';
-import ShortUrl from '../models/shortUrl';
-import getMetadata from '../utils/getMetadata';
-import generateUniqueShortUrl from '../utils/randomUrl';
-import toNewShortUrlEntry from '../utils/shortEntry';
-import truncateString from '../utils/truncateString';
+import shortUrl from '../services/shortUrl';
+import handleError from '../utils/handleError';
 
 const getAllShortUrls = async (_req: Request, res: Response): Promise<void> => {
   try {
-    const allUrls = await ShortUrl.find({});
+    const allUrls = await shortUrl.getAll();
     res.json(allUrls);
   } catch (error) {
-    res.status(400).send({ errorMessage: handleError(error) });
+    handleError(res, 'Failed to get all short URLs.', error);
   }
 };
 
 const addShortUrl = async (req: Request, res: Response): Promise<void> => {
   try {
-    const newShortUrlEntry = toNewShortUrlEntry(req.body);
-
-    //get metadata from  url
-    const metadata = await getMetadata(newShortUrlEntry.url);
-    console.log('metada', metadata);
-    let shortUrl = generateUniqueShortUrl();
-    while (await ShortUrl.findOne({ shortUrl })) {
-      shortUrl = generateUniqueShortUrl();
-    }
-
-    if (metadata.title) {
-      newShortUrlEntry.title = metadata.title;
-    }
-
-    if (metadata.image) {
-      newShortUrlEntry.image = metadata.image;
-    }
-    if (metadata.logo) {
-      newShortUrlEntry.logo = metadata.logo;
-    }
-    if (metadata.description) {
-      newShortUrlEntry.description = truncateString(metadata.description, 50);
-    }
-
-    const newEntry = new ShortUrl({
-      ...newShortUrlEntry,
-      shortUrl,
-    });
-
-    const savedEntry = await newEntry.save();
-
-    res.json(savedEntry);
+    const { url }: { url: String } = req.body;
+    const newShortUrlEntry = await shortUrl.create(url);
+    res.json(newShortUrlEntry);
   } catch (error) {
-    res.status(400).send({ errorMessage: handleError(error) });
+    handleError(res, 'Failed to add short URL.', error);
   }
 };
 
 const getFullUrl = async (req: Request, res: Response): Promise<void> => {
   try {
-    const shortUrl = req.params.shortUrl;
-    const entry = await ShortUrl.findOne({ shortUrl });
-
-    if (!entry) {
-      res
-        .status(404)
-        .send({ errorMessage: `Short URL not found for ${shortUrl}` });
-      return;
-    }
-
-    entry.totalClicks += 1;
-    await entry.save();
-    // res.redirect(301, entry.url);
-    res.send({ url: entry.url });
+    const url = req.params.shortUrl;
+    const entry = await shortUrl.get(url);
+    res.send(entry);
   } catch (error) {
-    res.status(400).send({ errorMessage: handleError(error) });
+    handleError(res, 'Failed to get full URL.', error);
   }
-};
-
-//![remove] Helper function
-const handleError = (error: unknown): string => {
-  let errorMessage = 'Something went wrong.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  return errorMessage;
 };
 
 export default {
-  addShortUrl,
   getAllShortUrls,
+  addShortUrl,
   getFullUrl,
 };
