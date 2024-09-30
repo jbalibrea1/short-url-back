b874e6e9718728bef359a8282e031067195c0fe9
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 01:06:21 +0200

    refactor: Update MongoDB connection and configuration
    Refactor the code in `db.ts` and `server.ts` to update the MongoDB connection and configuration.
    - Import `mongoose` using ES6 syntax in `db.ts`.
    - Add a check for the presence of the MongoDB URI in `db.ts` and exit the process if it is not provided.
    - Import `dotenv/config` in `server.ts` to load environment variables.
    - Add a new environment variable `DB_COLLECTION` in `server.ts`.
    
    This commit improves the MongoDB connection and configuration in the project.

--- back-express/src/config/db.ts
+++ back-express/src/config/db.ts
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = () => {
   mongoose.set('strictQuery', false);
 
   const url = process.env.MONGODB_URI;
   console.log('connecting to', url);
+  if (!url) {
+    console.log('No MongoDB URI provided');
+    process.exit(1);
+  }
 
   mongoose
     .connect(url)
--- back-express/src/config/server.ts
+++ back-express/src/config/server.ts
@@ -1,6 +1,7 @@
-require('dotenv').config();
+import 'dotenv/config';
 
 const PORT = process.env.PORT;
+const DB_COLLECTION = process.env.DB_COLLECTION;
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
@@ -9,4 +10,5 @@ const MONGODB_URI =
 export default {
   MONGODB_URI,
   PORT,
+  DB_COLLECTION,
 };
