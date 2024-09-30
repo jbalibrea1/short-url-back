39ffe72c75a0b422e169339544d7312f94486980
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 01:38:19 +0200

    refactor: Update GenerateUniqueShortID function to use MongoDB for checking uniqueness
    The GenerateUniqueShortID function in the utils package has been updated to use MongoDB for checking the uniqueness of the generated short ID. The function now takes a mongo.Collection as a parameter and uses it to query the database for existing short URLs. This change improves the efficiency and reliability of the ID generation process.

--- back-go/utils/generate_unique_id.go
+++ back-go/utils/generate_unique_id.go
@@ -1,30 +1,35 @@
 package utils
 
 import (
-	"log"
-	"net/http"
+	"context"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/jbalibrea1/short-url-back/back-go/models"
 	"github.com/teris-io/shortid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // GenerateUniqueShortID genera un ID corto único que no existe en la base de datos
-func GenerateUniqueShortID(c *gin.Context) (string, error) {
+func GenerateUniqueShortID(coll *mongo.Collection) (string, error) {
 	var sid string
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	for {
 		// Genera un nuevo ID corto
 		var newSid string
 		newSid, err := shortid.Generate()
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate short ID"})
-			log.Println("Failed to generate short ID:", err)
 			return "", err
 		}
 		sid = newSid
 
-		// Verifica si el ID corto generado ya existe en la base de datos
-		if !IsShortUrlExist(sid) {
+		var result models.ShortUrl
+
+		// Busca el ID corto en la base de datos
+		// Si no se encuentra el ID corto en la base de datos, es único, se rompe el bucle
+		if err = coll.FindOne(ctx, bson.M{"short_URL": sid}).Decode(&result); err != nil {
 			break
 		}
 	}
