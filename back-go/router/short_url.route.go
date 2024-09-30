0db2b3649857e827c182d3bbe3ad864dd02382a1
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 01:42:08 +0200

    ```text refactor: Move router setup to separate file and update routes
    The router setup code has been moved from the main.go file to a new file called short_url.route.go. This change improves code organization and separation of concerns. The routes have also been updated to use the ShortUrlController for handling requests. The /ping route has been kept for testing purposes.

---
+++ back-go/router/short_url.route.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jbalibrea1/short-url-back/back-go/controllers"
+)
+
+type ShortUrlRouterController struct {
+	shortUrlController controllers.ShortUrlController
+}
+
+func NewShortUrlRoutes(sucController controllers.ShortUrlController) ShortUrlRouterController {
+	return ShortUrlRouterController{sucController}
+}
+
+func (suc *ShortUrlRouterController) SetupRoutes(r *gin.Engine) {
+	// El controlador esta encapsulado en el router
+	//* gin pasa el contexto automáticamente a los controladores
+	//* por lo que no es necesario pasarlo manualmente
+	//* suc.shortUrlController.<controlador>(ctx)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+	r.GET("/shorturl", suc.shortUrlController.GetAllShortURLs)
+	r.GET("/shorturl/:shortURL", suc.shortUrlController.GetShortURL)
+	r.POST("/shorturl", suc.shortUrlController.CreateShortURL)
+}
