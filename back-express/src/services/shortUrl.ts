53f2b3f53b7ad93343ee760512063416103a95fa
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 01:09:09 +0200

    refactor: Add shortUrl service to handle URL creation and retrieval
    This commit adds a new file, shortUrl.ts, which contains the implementation of the shortUrl service. The service provides functions for creating and retrieving short URLs.
    
    The getAll function retrieves all existing short URLs from the database. The create function generates a unique short URL for a given long URL and saves it to the database along with metadata obtained from the URL. The get function retrieves a short URL entry based on the provided short URL.
    
    These changes improve the organization and separation of concerns in the codebase by moving the logic related to short URLs into a dedicated service.

---
+++ back-express/src/services/shortUrl.ts
@@ -0,0 +1,52 @@
+import ShortUrlModel from '../models/shortUrlModel';
+import getMetadata from '../utils/getMetadata';
+import generateUniqueShortUrl from '../utils/randomUrl';
+import toNewShortUrlEntry from '../utils/shortEntry';
+import truncateString from '../utils/truncateString';
+
+const getAll = async () => {
+  const allUrls = await ShortUrlModel.find({});
+  return allUrls;
+};
+
+const create = async (url: String) => {
+  const newShortUrlEntry = toNewShortUrlEntry(url);
+
+  //get metadata from  url
+  const metadata = await getMetadata(newShortUrlEntry.url);
+
+  // generate a unique short url while not clashing with existing ones
+  let shortUrl = generateUniqueShortUrl();
+  while (await ShortUrlModel.findOne({ shortUrl })) {
+    shortUrl = generateUniqueShortUrl();
+  }
+
+  if (metadata.title) newShortUrlEntry.title = metadata.title;
+  if (metadata.logo) newShortUrlEntry.logo = metadata.logo;
+  if (metadata.description)
+    newShortUrlEntry.description = truncateString(metadata.description, 50);
+
+  // create a new entry
+  const newEntry = new ShortUrlModel({
+    ...newShortUrlEntry,
+    shortUrl,
+  });
+  const savedEntry = await newEntry.save();
+
+  return savedEntry;
+};
+
+const get = async (shortUrl: String) => {
+  const entry = await ShortUrlModel.findOne({ shortUrl });
+
+  if (!entry) {
+    //!!!!!!!!!!!!!!!!!!!!!!!!!
+    throw new Error(`Short URL not found for ${shortUrl}`);
+  }
+
+  entry.totalClicks += 1;
+  await entry.save();
+  return entry;
+};
+
+export default { getAll, create, get };
